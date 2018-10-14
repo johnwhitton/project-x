ccd4cb30a5f21d33b43f00eda30f274a37c0409d
juliusbuckley <juliusbuckley@gmail.com>
2018-10-14 13:31:10 -0700

    create number formatter for usd and asset

--- src/TableRow.js
+++ src/TableRow.js
@@ -1,18 +1,22 @@
 import React from 'react';
  
- const formatter = new Intl.NumberFormat('en-US', {
+ const USDFormatter = new Intl.NumberFormat('en-US', {
    style: 'currency',
    currency: 'USD',
    minimumFractionDigits: 2,
+ });
+
+ const AssetFormatter = new Intl.NumberFormat('en-US', {
+   minimumFractionDigits: 3,
  });
 
 const TableRow = ({data}) => {
   return (
     <div className='table-row-container'>
       <div className='row-item row-asset'>{data.asset}</div>
-      <div className='row-item row-quantity'>{`${data.quantity} ${data.symbol}`}</div>
-      <div className='row-item row-price'>{formatter.format(data.price)}</div>
-      <div className='row-item row-total'>{formatter.format(data.total)}</div>
+      <div className='row-item row-quantity'>{`${AssetFormatter.format(data.quantity)} ${data.symbol}`}</div>
+      <div className='row-item row-price'>{USDFormatter.format(data.price)}</div>
+      <div className='row-item row-total'>{USDFormatter.format(data.total)}</div>
     </div>
   )
 };
