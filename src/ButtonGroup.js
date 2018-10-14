56d4a479d05eb0fa65089b7af09e788e86ed0a53
juliusbuckley <juliusbuckley@gmail.com>
2018-10-14 13:11:35 -0700

    add icons to buttons

--- src/ButtonGroup.js
+++ src/ButtonGroup.js
@@ -3,9 +3,9 @@ import React from 'react';
 const ButtonGroup = () => {
   return (
     <div className='button-group-container'>
-      <button className='btn btn-create-shell'>Create Shell</button>
-      <button className='btn btn-receive'>Recieve</button>
-      <button className='btn btn-send'>Send</button>
+      <button className='btn btn-create-shell'><i className="fas fa-plus btn-icon"></i>Create Shell</button>
+      <button className='btn btn-receive'><i className="fas fa-qrcode btn-icon"></i>Receive</button>
+      <button className='btn btn-send'><i className="far fa-check-circle btn-icon"></i>Send</button>
     </div>
   )
 };
