1aad0887f436d343186e3f1aedbd10745969a6cc
juliusbuckley <juliusbuckley@gmail.com>
2018-10-14 15:35:05 -0700

    create logic to toggle additional tokens

---
+++ src/CowriRow.js
@@ -0,0 +1,24 @@
+import React from 'react';
+ 
+ const USDFormatter = new Intl.NumberFormat('en-US', {
+   style: 'currency',
+   currency: 'USD',
+   minimumFractionDigits: 2,
+ });
+
+ const AssetFormatter = new Intl.NumberFormat('en-US', {
+   minimumFractionDigits: 3,
+ });
+
+const TableRow = ({data}) => {
+  return (
+    <div className='table-row-container cowri-table-row'>
+      <div className='cowri-row-item row-asset'>{data.asset}</div>
+      <div className='cowri-row-item row-quantity'>{`${AssetFormatter.format(data.quantity)} ${data.symbol}`}</div>
+      <div className='cowri-row-item row-price'>{USDFormatter.format(data.price)}</div>
+      <div className='cowri-row-item row-total'>{USDFormatter.format(data.total)}</div>
+    </div>
+  )
+};
+
+export default TableRow;
--- src/Table.js
+++ src/Table.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
+import CowriRow from './CowriRow.js';
 import TableRow from './TableRow.js';
 import TableColumn from './TableColumn.js';
 
-const Table = ({columns, rows}) => {
-  return (
-    <div className='table-container'>
-      <TableColumn data={columns}/>
-        {Object.keys(rows).map(row => (
+class Table extends Component {
+  state = {
+    showTokens: false,
+  }
+  toggleTokenStatus = () =>{
+    console.log('here', this)
+    this.setState(prevState => ({
+      showTokens: !prevState.showTokens
+    }));
+  }
+  render() {
+    const {columns, rows} = this.props;
+    const {showTokens} = this.state;
+    const cowriRow = {
+      asset: 'Cowri', quantity: 31530.427222, symbol: 'wri', price: 1, total: 12051.91, address: '0x000'
+    };
+    const toggleFooterText = showTokens ? 'Hide' : 'Show';
+    return (
+      <div className='table-container'>
+        <TableColumn data={columns}/>
+        <CowriRow data={cowriRow} key={cowriRow.address}/>
+        {this.state.showTokens && Object.keys(rows).map(row => (
           <TableRow data={rows[row]} key={rows[row].address}/>
         ))}
-    </div>
-  )
-};
+          <div 
+            className='table-footer-message'
+            onClick={this.toggleTokenStatus}>
+              <i className="fas fa-coins footer-icon"></i>
+              <span>{`${toggleFooterText} additional tokens`}</span>
+          </div>
+      </div>
+    )
+  }
+}
 
 export default Table;
