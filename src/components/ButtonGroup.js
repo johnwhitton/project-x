a176ed335abbfa3dc69107856d0c45debe5705c0
juliusbuckley <juliusbuckley@gmail.com>
2018-10-21 18:30:02 -0700

    update html and css for send modal

--- src/components/ButtonGroup.js
+++ src/components/ButtonGroup.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import SendModal from '../modals/SendModal';
 
 class ButtonGroup extends Component {
   state = {
@@ -15,7 +14,6 @@ class ButtonGroup extends Component {
   render() {
     return (
       <div>
-        {this.state.isSendModalVisible && <SendModal/>}
         <div className='button-group-container'>
           <button className='btn btn-create-shell'><i className="fas fa-plus btn-icon"></i>Create Shell</button>
           <button className='btn btn-receive'><i className="fas fa-qrcode btn-icon"></i>Receive</button>
--- src/modals/SendModal.js
+++ src/modals/SendModal.js
@@ -9,24 +9,24 @@ class SendModal extends Component {
   render() {
     return (
      <div className='send-modal-container'>
-      <form className='send-modal-form' action=''>
+      <form className='send-modal-form' action='' autocomplete='false' autofill='false'>
         <div className='send-modal-form-container'>
           <div className='send-modal-input address-input'>
             <label htmlFor='wallet-address'><i className="far fa-check-circle btn-icon"></i>Send from Metamask Wallet</label>
-            <input type='text' name='wallet-address' id='wallet-address'/>
+            <input type='text' className='wallet-address' placeholder='Cowri' disabled/>
           </div>
           <div className='send-modal-input recipient-input'>
             <label htmlFor='recipient-address'>Recipient Address</label>
-            <input type='text' name='recipient-address' id='recipient-address'/>
+            <input type='text' className='recipient-address' placeholder='0x...'/>
           </div>
           <div className='send-modal-input amount-input'>
             <label htmlFor='cowri-amount'>Amount</label>
-            <input type='text' name='cowri-amount' id='cowri-amount'/>
+            <input type='text' className='cowri-amount' placeholder='0.0'/>
           </div>
           <div className='send-button-group-container'>
-            <button className='btn btn-cancel no-margin' onClick={() => ''}>Cancel</button>
-            <span>Fee: $0.014</span>
-            <button className='btn btn-send no-margin' onClick={() => ''}>
+            <button className='btn btn-cancel no-margin' value='submit' onClick={(e) => e.preventDefault()}>Cancel</button>
+            <span className='send-modal-fee'>Fee: $0.014</span>
+            <button className='btn btn-send no-margin' value='submit' onClick={(e) => e.preventDefault()}>
               <i className="far fa-check-circle btn-icon"></i>
               Send
             </button>
