7b5f44c098c8a2c8635cca28592d2a1c68965432
Lastrellik <lastrellik@gmail.com>
2019-02-10 17:56:44 -0800

    Pull out some debug logic and fix and add tests for sending tokens

--- src/models/Constants.js
+++ src/models/Constants.js
@@ -2,3 +2,6 @@ export const ExpirationTimeInSeconds = '' + (Date.now() + 3600000);
 export const NULL_ADDRESS = '0x0000000000000000000000000000000000000000';
 export const DECIMALS = 18;
 export const RPC_URL = 'http://localhost:8545';
+export const KOVAN_NETWORK_ID = 42;
+export const SHELL_MAPPING_CONTRACT_ADDRESS =
+  '0xc8f91597515f5baa3ec9767b34b517cdeb839855';
--- src/models/provider/CowriProvider.js
+++ src/models/provider/CowriProvider.js
@@ -1,47 +1,52 @@
+import {
+  BigNumber,
+  ContractWrappers,
+  generatePseudoRandomSalt,
+  signatureUtils,
+  Web3ProviderEngine,
+  RPCSubprovider,
+} from '0x.js';
+import {getContractAddressesForNetworkOrThrow} from '@0x/contract-addresses';
+import {Web3Wrapper} from '@0x/web3-wrapper';
+
 import {Provider} from './Provider';
 import ABI from '../../ABI';
 import ShellABI from '../../ShellABI';
 import AxiosRequest from '../request/axiosRequest';
 import * as credentials from '../../../config/parityPassword.json';
-
 import {
-  assetDataUtils,
-  BigNumber,
-  ContractWrappers,
-  generatePseudoRandomSalt,
-  Order,
-  orderHashUtils,
-  signatureUtils,
-  Web3ProviderEngine,
-  RPCSubprovider,
-} from '0x.js';
+  DECIMALS,
+  KOVAN_NETWORK_ID,
+  RPC_URL,
+  SHELL_MAPPING_CONTRACT_ADDRESS,
+  NULL_ADDRESS,
+} from '../Constants';
 import {getFutureExpiration} from '../../utils/utils';
-import {getContractAddressesForNetworkOrThrow} from '@0x/contract-addresses';
-import {Web3Wrapper} from '@0x/web3-wrapper';
 
-import {NULL_ADDRESS} from '../Constants';
-
-export class CowriProvider extends Provider {
+export default class CowriProvider extends Provider {
   constructor() {
     super();
     const Web3 = require('web3');
-    this.web3 = new Web3('http://localhost:8545');
+    this.web3 = new Web3(RPC_URL);
     this.providerEngine = new Web3ProviderEngine();
-    this.providerEngine.addProvider(
-      new RPCSubprovider('http://localhost:8545'),
-    );
+    this.providerEngine.addProvider(new RPCSubprovider(RPC_URL));
     this.providerEngine.start();
     this.web3Wrapper = new Web3Wrapper(this.providerEngine);
     this.contractWrappers = new ContractWrappers(this.providerEngine, {
-      networkId: 42,
+      networkId: KOVAN_NETWORK_ID,
     });
     this.request = new AxiosRequest();
   }
 
   sendToken = async (senderAddress, receiverAddress, token) => {
-    const tokenContract = await new web3.eth.Contract(ShellABI, token.Address);
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000000000;
+    await this.unlockAccount(senderAddress);
+    const tokenContract = await new this.web3.eth.Contract(
+      ShellABI,
+      token.address,
+    );
     await tokenContract.methods
-      .transfer(receiverAddress, tokenDecimalAmount)
+      .transfer(receiverAddress, '' + token.toBaseUnitAmount())
       .send({from: senderAddress});
   };
 
@@ -51,12 +56,10 @@ export class CowriProvider extends Provider {
     senderAddress,
     receiverAddress,
   ) => {
-    const kovanNetworkID = 42;
     const contractAddresses = getContractAddressesForNetworkOrThrow(
-      kovanNetworkID,
+      KOVAN_NETWORK_ID,
     );
     const exchangeAddress = contractAddresses.exchange;
-    console.log('exchange address: ' + exchangeAddress);
     jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000000000;
 
     const order = {
@@ -74,15 +77,12 @@ export class CowriProvider extends Provider {
       makerFee: new BigNumber(0),
       takerFee: new BigNumber(0),
     };
-    console.log('unlocking accounts');
     await this.unlockAccount(senderAddress);
     await this.unlockAccount(receiverAddress);
-    console.log('maker approval hash');
     const makerApprovalTxHash = await this.contractWrappers.erc20Token.setUnlimitedProxyAllowanceAsync(
       tokenToSend.address,
       senderAddress,
     );
-    console.log('taker approval hash');
     const takerApprovalTxHash = await this.contractWrappers.erc20Token.setUnlimitedProxyAllowanceAsync(
       tokenToReceive.address,
       receiverAddress,
@@ -91,12 +91,9 @@ export class CowriProvider extends Provider {
       this.web3Wrapper.awaitTransactionSuccessAsync(makerApprovalTxHash),
       this.web3Wrapper.awaitTransactionSuccessAsync(takerApprovalTxHash),
     ]);
-    console.log('unlocking accounts');
     await this.unlockAccount(senderAddress);
     await this.unlockAccount(receiverAddress);
-    console.log('signing Order');
     const signedOrder = await this.signOrder(order, senderAddress);
-    console.log('validating order fillable');
     await this.contractWrappers.exchange.validateOrderFillableOrThrowAsync(
       signedOrder,
     );
@@ -107,14 +104,14 @@ export class CowriProvider extends Provider {
     const params = [accountPublicKey, credentials.password, null];
     const rawRequest = {
       method: 'personal_unlockAccount',
-      params: params,
+      params,
       id: 1,
       jsonrpc: '2.0',
     };
     await this.request.postAsync(
-      'http://localhost:8545',
+      RPC_URL,
       JSON.stringify(rawRequest),
-      res => console.log('res' + res),
+      res => res,
     );
   };
 
@@ -127,9 +124,7 @@ export class CowriProvider extends Provider {
         gasLimit: 1000000,
       },
     );
-    console.log('txHash: ' + txHash);
     await this.web3Wrapper.awaitTransactionSuccessAsync(txHash);
-    console.log('Transaction success');
     return txHash;
   };
 
@@ -145,25 +140,22 @@ export class CowriProvider extends Provider {
 
   // Returns an object mapping the address of each token to their balance in wei
   getTokenBalances = async accountPublicKey => {
-    const SHELL_MAPPING_CONTRACT_ADDRESS =
-      '0xc8f91597515f5baa3ec9767b34b517cdeb839855';
-    const accountAPublicKey = '0x141cc50934F7911D7f85051A7F3C14c70b6BcE3C';
     const tokenBalances = {};
     const shellMappingContract = await new this.web3.eth.Contract(
       ABI,
       SHELL_MAPPING_CONTRACT_ADDRESS,
     );
-    for (var i = 0; i < 6; i++) {
-      var contractAddress = await shellMappingContract.methods
-        .shellMap(accountAPublicKey, i)
+    for (let i = 0; i < 6; i++) {
+      const contractAddress = await shellMappingContract.methods
+        .shellMap(accountPublicKey, i)
         .call();
       const tokenContract = await new this.web3.eth.Contract(
         ShellABI,
         contractAddress,
       );
-      tokenBalances[
-        contractAddress
-      ] = await await tokenContract.methods.balanceOf(accountAPublicKey).call();
+      tokenBalances[contractAddress] = await tokenContract.methods
+        .balanceOf(accountPublicKey)
+        .call();
     }
     return tokenBalances;
   };
